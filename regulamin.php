5460d2cd9f3ab47dd5d8b0d922addd36e9c5a125
mginter3 <gintermateusztp@gmail.com>
2025-05-12 23:09:13 +0200

    dodanie podstrony regulamin.php

---
+++ regulamin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Regulamin – Portal z ofertami pracy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_regulamin.css">
+</head>
+<body>
+
+<header>
+    <h1>Portal z ofertami pracy w Dojczlandzie</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Strona główna</a></li>
+            <li><a href="lista_ofert.php">Oferty pracy</a></li>
+            <li><a href="dodaj_oferte.php">Dodaj ofertę</a></li>
+            <li><a href="konto.php">Moje konto</a></li>
+            <li><a href="rejestracja.php">Rejestracja</a> / <a href="logowanie.php">Logowanie</a></li>
+            <li><a href="kontakt.php">Kontakt</a></li>
+            <li><a href="o_nas.php">O nas</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+
+<footer>
+    <p>&copy; 2025 ginterkoceniakXDDDDD3wnocy – Wszystkie prawa zastrzeżone</p>
+    <a href="regulamin.php">Regulamin</a> | <a href="polityka_prywatnosci.php">Polityka prywatności</a>
+</footer>
+
+</body>
+</html>
